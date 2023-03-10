da54332ded4212a1d6b8e6c08846fb110ca42e76
farzeena123 <farzeena2000@gmail.com>
2023-03-10 20:01:26 +0300

    Updates

---
+++ precache-manifest.9fc1d09d9e4adc5f7c02995bd7d16df9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b0d2494f4a29112e0f3",
+    "url": "/elRed/static/css/main.fc9c1b03.chunk.css"
+  },
+  {
+    "revision": "1b0d2494f4a29112e0f3",
+    "url": "/elRed/static/js/main.1b0d2494.chunk.js"
+  },
+  {
+    "revision": "a23dc6c2761d48e5a4d0",
+    "url": "/elRed/static/css/1.c98d0d10.chunk.css"
+  },
+  {
+    "revision": "a23dc6c2761d48e5a4d0",
+    "url": "/elRed/static/js/1.a23dc6c2.chunk.js"
+  },
+  {
+    "revision": "4265f3133d1f4240d8e0",
+    "url": "/elRed/static/js/runtime~main.4265f313.js"
+  },
+  {
+    "revision": "493a930a197882acd0c2ec5cc2dd9c81",
+    "url": "/elRed/static/media/LOGO.493a930a.jpg"
+  },
+  {
+    "revision": "912a974a358f2d0abc4a9a6f5c50f539",
+    "url": "/elRed/index.html"
+  }
+];
